5025a2ab61ac449e3440a5e91f798512eb377005
diegolu7 <diegolu7@gmail.com>
2022-07-31 02:04:35 -0300

    fix console.log

--- assets/js/vacio.js
+++ assets/js/vacio.js
@@ -3,8 +3,8 @@ export default function vacio(){
   let cantidades = obtener_localStorage('cantidades');
   const even = (element) => element == 0;
   cantidades.every(even);
-  console.log(cantidades);
-  console.log(cantidades.some(even));
+  //console.log(cantidades);
+  //console.log(cantidades.some(even));
   if(cantidades.every(even)){
     //alert("Esta vacio ves? no hay productos en el carro!");
       Swal.fire({
